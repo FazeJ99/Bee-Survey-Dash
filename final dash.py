c8989125f7e6dd635a3a7c3a1d6c034541686efb
Justin Ngugi <justsane12@gmail.com>
2024-09-30 12:11:06 +0300

    Combining the two dashboards

--- final dash.py
+++ final dash.py
@@ -8,10 +8,9 @@ app = Dash(__name__)
 df = pd.read_csv("Dataset/intro_bees.csv")
 #df = pd.read_csv("https://raw.githubusercontent.com/LubangaD/beesurveydashboard/refs/heads/main/Dataset/intro_bees.csv")
 
+
 df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of Colonies Impacted']].mean()
 df.reset_index(inplace=True)
-print(df.head())
-df_sorted = df.sort_values(by='Pct of Colonies Impacted', ascending=False)
 
 app.layout = html.Div(children=[html.H1(children='Bees Bar Plot')  ,
           dcc.Graph(
@@ -27,8 +26,22 @@ app.layout = html.Div(children=[html.H1(children='Bees Bar Plot')  ,
                   )
               }
           )                      
-                                ])
+                                ]),
+html.Div(children=[html.H1(children = 'Bees Line Plot') ,
+                      dcc.Graph(
+                          id = 'line-plot',
+                          figure = {
+                              'data' : [
+                                  go.Line(
+                                  x = df['Year'],
+                                  y = df['Pct of Colonies Impacted'],
+                                  marker = dict(color = 'indigo')
+                              )
+                              ],
+                          }
+                      )
+                      ]
+                      )
 
 if __name__ == '__main__':
     app.run_server(debug = True)
-    
