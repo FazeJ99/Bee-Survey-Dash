98186d6de4298d5b6d16e4c7f3c002ecf1516abf
Justin Ngugi <justsane12@gmail.com>
2024-10-01 10:09:13 +0300

    Final dash addition

--- line.py
+++ line.py
@@ -12,15 +12,19 @@ df = df.groupby(['State', 'ANSI', 'Affected by', 'Year', 'state_code'])[['Pct of
 df.reset_index(inplace=True)
 print(df.head()) 
 
+dff = df.copy()
+dff = dff[(dff['State'] == 'Idaho') | (dff['State'] == "New York") | (dff['State'] == "New Mexico")]
+dff = dff[(dff['Year'] == 2015) | (dff['Year'] == 2016) | (dff['Year'] == 2017)]
+
 app.layout = html.Div(children=[html.H1(children = 'Bees Line Plot') ,
                       dcc.Graph(
                           id = 'line-plot',
                           figure = {
                               'data' : [
                                   go.Line(
-                                  x = df['Year'],
-                                  y = df['Pct of Colonies Impacted'],
-                                  marker = dict(color = 'indigo')
+                                  x = dff['Year'],
+                                  y = dff['Pct of Colonies Impacted'],
+                                  color = 'Affected by'
                               )
                               ],
                           }
